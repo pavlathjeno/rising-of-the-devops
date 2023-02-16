aa27fa461a337e819f27a212a7d9554e9b46bf34
pavlathjeno <pavlathjeno@gmail.com>
2023-02-16 18:42:01 +0100

    Fix : Missing semicolons

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
