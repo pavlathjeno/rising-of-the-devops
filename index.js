7141b5c3e2a01f10f90a39d3313fa09abb6c2594
pavlathjeno <pavlathjeno@gmail.com>
2023-02-16 18:53:09 +0100

    Fix : Semicolon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
